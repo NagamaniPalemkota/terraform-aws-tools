e8311b2679c592312f9037459da0b06e6daeba5f
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-08-06 14:36:58 +0530

    committing resources for jenkins

--- cicd/provider.tf
+++ cicd/provider.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
 
 		bucket = "muvva-remotestate-bucket"			#this is the s3 bucket name
-		key = "jenkins"				#this is the user defined key name for bucket
+		key = "jenkins1"				#this is the user defined key name for bucket
 		dynamodb_table = "muvva-lock"					#mentioning the dynamo table name use for locking
 		region = "us-east-1"
 	  }
